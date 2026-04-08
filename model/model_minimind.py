f22cd89f082ce0af6614f4bd763ad6b6a2ed7f85
white <qwhite@163.com>
2026-04-08 17:39:44 +0800

    自动计算注意力与手动计算主力修改

--- model/model_minimind.py
+++ model/model_minimind.py
@@ -1,7 +1,7 @@
 import math
+
 import torch
 import torch.nn.functional as F
-
 from torch import nn
 from transformers import PreTrainedModel, GenerationMixin, PretrainedConfig
 from transformers.activations import ACT2FN
@@ -316,12 +316,25 @@ class Attention(nn.Module):
 
         # 如果需要缓存，保存当前 KV
         past_kv = (xk, xv) if use_cache else None
-        xq, xk, xv = (xq.transpose(1, 2), repeat_kv(xk, self.n_rep).transpose(1, 2), repeat_kv(xv, self.n_rep).transpose(1, 2))
-        if self.flash and (seq_len > 1) and (not self.is_causal or past_key_value is None) and (attention_mask is None or torch.all(attention_mask == 1)):
-            output = F.scaled_dot_product_attention(xq, xk, xv, dropout_p=self.dropout if self.training else 0.0, is_causal=self.is_causal)
+        xq, xk, xv = (
+            xq.transpose(1, 2),
+            repeat_kv(xk, self.n_rep).transpose(1, 2),
+            repeat_kv(xv, self.n_rep).transpose(1, 2)
+        )
+        if (self.flash and (seq_len > 1) and
+                (not self.is_causal or past_key_value is None) and
+                (attention_mask is None or torch.all(attention_mask == 1))):
+            output = F.scaled_dot_product_attention(xq, xk, xv,
+                                                    dropout_p=self.dropout if self.training else 0.0,
+                                                    is_causal=self.is_causal)
         else:
             scores = (xq @ xk.transpose(-2, -1)) / math.sqrt(self.head_dim)
-            if self.is_causal: scores[:, :, :, -seq_len:] += torch.full((seq_len, seq_len), float("-inf"), device=scores.device).triu(1)
+            if self.is_causal:
+                scores[:, :, :, -seq_len:] += torch.full(
+                    (seq_len, seq_len),
+                    float("-inf"),
+                    device=scores.device
+                ).triu(1)
             if attention_mask is not None: scores += (1.0 - attention_mask.unsqueeze(1).unsqueeze(2)) * -1e9
             output = self.attn_dropout(F.softmax(scores.float(), dim=-1).type_as(xq)) @ xv
         output = output.transpose(1, 2).reshape(bsz, seq_len, -1)
